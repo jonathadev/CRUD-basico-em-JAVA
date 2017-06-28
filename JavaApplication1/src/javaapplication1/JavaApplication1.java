15dd2e450c658cbdc29e75dbc8c778f9462f2c14
jonathadev <jonatha.mendonca@hotmail.com>
2017-06-28 14:21:31 -0300

    first commit CRUD JAVA para processso

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import apresentacao.JFrameAluno;
+
+/**
+ *
+ * @author ADM
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        JFrameAluno jFrameAluno = new JFrameAluno(); //Cria o objeto Janela
+        jFrameAluno.setLocationRelativeTo(null); //Coloca a janela no centro da tela
+        jFrameAluno.setVisible(true); 
+// Mostra a janela// TODO code application logic here
+    }
+    
+}
